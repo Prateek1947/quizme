a9b475a221218ca2b8c4051509b6d25222efe90b
Prateek <psachan@cs.iitr.ac.in>
2019-06-28 23:20:49 +0530

    Changed all views to class-based

--- problems/views.py
+++ problems/views.py
@@ -1,14 +1,96 @@
-from django.shortcuts import render
-from .serializers import ProblemsSerializer
+from django.shortcuts import render, get_object_or_404
+from .serializers import *
 from rest_framework.views import APIView
-from rest_framework import serializers,response
+from rest_framework import serializers, response, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from . import models
+from django.http import JsonResponse, HttpResponse
+from rest_framework.permissions import *
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.models import Token
+
 
 # Create your views here.
 
 
-class Problems(APIView):
-    def get(self,request):
-        problems=models.Problem.objects.all()
-        serializer=ProblemsSerializer(problems, many=True)
-        return response.Response(serializer.data)
+class ProblemList(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
+
+    def get(self, request):
+        problems = models.Problem.objects.all()
+        serializer = ProblemsListSerializer(problems, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProblemsListSerializer(data=request.data)
+        serializer.author = request.user.username
+        print(serializer.is_valid())
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProblemDetail(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def get_object(self, pk):
+        return get_object_or_404(models.Problem, pk=pk)
+
+    def get(self, request, pk):
+        prob = self.get_object(pk)
+        self.check_object_permissions(request, prob)
+        serializer = ProblemDetailSerializer(prob)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        prob = self.get_object(pk)
+        self.check_object_permissions(request, prob)
+        serializer = ProblemDetailSerializer(prob, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        prob = self.get_object(pk)
+        self.check_object_permissions(request, prob)
+        prob.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+def Authors(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+
+class LogoutView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        user = request.user
+        user.auth_token.delete()
+        return JsonResponse({'detail': "user logged out"}, status=status.HTTP_200_OK)
+
+
+class ProfileView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
