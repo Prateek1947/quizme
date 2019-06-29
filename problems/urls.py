4b3af66ec47dc09de494f992e72fde2f8bb4251b
Prateek <psachan@cs.iitr.ac.in>
2019-06-29 23:15:17 +0530

    Removed user url patterns

--- problems/urls.py
+++ problems/urls.py
@@ -6,9 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('problems/', views.ProblemList.as_view()),
     path('problem/<int:pk>/', views.ProblemDetail.as_view()),
-    path('users/', views.Authors),
     path('login/', obtain_auth_token),
     path('logout/', views.LogoutView.as_view()),
-    path('profile/', views.ProfileView.as_view())
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
