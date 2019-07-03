bb94bbb5ea76a40af1d6288fc940b932781926cb
Prateek <psachan@cs.iitr.ac.in>
2019-07-03 15:08:16 +0530

    Adds answer field and feature to hash answers

--- problems/models.py
+++ problems/models.py
@@ -2,6 +2,8 @@ import datetime
 
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import hashers
+from django.shortcuts import get_object_or_404
 
 
 # Create your models here.
@@ -21,8 +23,21 @@ class Problem(models.Model):
     tags = models.ManyToManyField(Tag, related_name='problems')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True,
                                related_name='contributions')
-    solvers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='solves')
+    solvers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='solves', blank=True)
     solved = models.BooleanField(default=False)
+    answer = models.CharField(max_length=128, blank=True, )
 
     def __str__(self):
         return self.title
+
+    def set_answer(self, raw_answer):
+        self.answer = hashers.make_password(raw_answer, salt='hello')
+
+    def save(self, **kwargs):
+        if self.pk is None:
+            self.set_answer(self.answer)
+        else:
+            problem = get_object_or_404(Problem, pk=self.pk)
+            if problem.answer != self.answer:
+                self.set_answer(self.answer)
+        super().save(**kwargs)
