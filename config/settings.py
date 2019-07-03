daf8735e8adedf9d9d6df01121a4fa937a0415f0
Prateek <psachan@cs.iitr.ac.in>
2019-07-03 15:18:08 +0530

    Adds submissions in installed apps

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'problems.apps.ProblemsConfig',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'submission.apps.SubmissionConfig'
 ]
 
 MIDDLEWARE = [
