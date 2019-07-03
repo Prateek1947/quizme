95cb7ef63f84efd57139d12f851db076b04b4cd3
Prateek <psachan@cs.iitr.ac.in>
2019-07-03 15:17:18 +0530

    Adds urlpatterns for submission app

--- config/urls.py
+++ config/urls.py
@@ -17,13 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.conf.urls.static import static
-from rest_framework.authtoken import views
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
     path('api/', include('problems.urls')),
-    path('api/users/', include('user.urls'))
+    path('api/users/', include('user.urls')),
+    path('api/submission/', include('submission.urls'))
 ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
