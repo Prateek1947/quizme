caf9f7d0e45661dcee5dbb51da75242740af4513
Prateek <psachan@cs.iitr.ac.in>
2019-07-11 23:16:28 +0530

    Added feature to change filename before upload

--- user/models.py
+++ user/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
+import os
 
 
 # Create your models here.
+def rename_file(instance, filename):
+    extension = filename.split('.')[-1]
+    file = str(uuid.uuid4()) + '.' + extension
+    return os.path.join('users', file)
+
 
 class User(AbstractUser):
     following = models.ManyToManyField('self', related_name='followers', symmetrical=False, blank=True)
-    profile_picture = models.ImageField(blank=True, upload_to='users')
+    profile_picture = models.ImageField(blank=True, upload_to=rename_file)
